5a7d10f4c503fac772da332d5f7f31b90f73fc53
Smr2dhi <smr2dhiya@gmail.com>
2026-09-15 18:50:07 +0530

    .

--- app/grep_tool.py
+++ app/grep_tool.py
@@ -2,3 +2,32 @@ import subprocess
 from agents import function_tool
 
 from app.config import DOCUMENTS_DIR
+
+@function_tool
+def grep_search(keywords:list[str])-> str:
+    command=[
+         "rg",
+        "-n",
+        "-i",
+        "-F"
+    ]
+
+    for keyword in keywords:
+        command.extend(["-e", keyword])
+
+    command.append(DOCUMENTS_DIR)
+
+    result = subprocess.run(
+        command,
+        capture_output=True,
+        text=True,
+        check=False
+    )
+
+    if result.returncode == 1:
+        return "No matching results found."
+
+    if result.returncode == 2:
+        return f"ripgrep error: {result.stderr}"
+
+    return result.stdout
